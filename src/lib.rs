ea074b5ee2f15e642b72f140305e350e44519f03
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-02 17:51:24 +0100

    Adds assets and command line parsing

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub fn pcompose() {
+    println!("Executing pcompose()");
+}
+
+pub fn pconvert() {
+    println!("Executing pconvert()");
+}
+
+pub fn pbenchmark() {
+    println!("Executing pbenchmark()");
+}
+
+pub fn popencl() {
+    println!("Executing popencl()");
+}
+
+pub fn pversion() {
+    println!("Executing pversion()");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
+fn print_usage() {
+    println!(
+        "Usage: pconvert-rust <command> [args...]\nwhere command can be one of the following: compose, convert, benchmark, opencl, version"
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next(); // skip program name
+
+    match args.next() {
+        Some(action) => match &action[..] {
+            "compose" => pconvert_rust::pcompose(),
+            "convert" => pconvert_rust::pconvert(),
+            "benchmark" => pconvert_rust::pbenchmark(),
+            "opencl" => pconvert_rust::popencl(),
+            "version" => pconvert_rust::pversion(),
+            _ => print_usage(),
+        },
+        _ => print_usage(),
+    }
 }
