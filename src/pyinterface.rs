395b4f5cef5fdc8a15a3fdf9d25caf1a3f8b9f5d
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-07 15:42:14 +0100

    Optional arguments and defaults for blend_images

--- src/pyinterface.rs
+++ src/pyinterface.rs
@@ -3,8 +3,26 @@ use pyo3::types::PyTuple;
 use pyo3::wrap_pyfunction;
 
 #[pyfunction]
-fn blend_images(img1_path: String, img2_path: String, out_path: String) {
-    println!("Blend images: {} - {} - {}", img1_path, img2_path, out_path);
+fn blend_images(bot_path: String, top_path: String, target_path: String, algorithm: Option<String>, is_inline: Option<bool>) {
+    let algorithm = match algorithm {
+        Some(algorithm) => algorithm,
+        None => {
+            let default = "multiplicative";
+            println!("Algorithm not specified, using default: {}", default);
+            String::from(default)
+        }
+    };
+
+    let is_inline = match is_inline {
+        Some(is_inline) => is_inline,
+        None => {
+            let default = false;
+            println!("Indication to use inline methods not specified, using default: {}", default);
+            default
+        }
+    };
+
+    println!("Blend images: {} - {} - {} - {:?} - {:?}", bot_path, top_path, target_path, algorithm, is_inline);
 }
 
 #[pyfunction]
