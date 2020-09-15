b34ddb87ad919871c5da748dd375aa6ef7046fa3
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-15 14:59:42 +0100

    ThreadPool foundation

--- src/parallelism.rs
+++ src/parallelism.rs
@@ -1,16 +1,61 @@
-use std::sync::mpsc;
-use std::thread::JoinHandle;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread::{spawn, JoinHandle};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
     sender: mpsc::Sender<Message>,
 }
 
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            workers.push(Worker::new(Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, func: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let task = Box::new(func);
+        self.sender.send(Message::NewTask(task)).unwrap();
+    }
+}
+
 struct Worker {
     thread: Option<JoinHandle<()>>,
 }
 
-type Task = Box<dyn FnOnce()>;
+impl Worker {
+    fn new(receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewTask(task) => {
+                    task();
+                }
+
+                Message::Terminate => {
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            thread: Some(thread),
+        }
+    }
+}
+
+type Task = Box<dyn FnOnce() + Send>;
 enum Message {
     NewTask(Task),
     Terminate,
