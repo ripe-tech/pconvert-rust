b836285fb3d22c33e12a189dd1d5001a99e9a5f5
João Magalhães <joamag@gmail.com>
2020-09-16 09:39:31 +0100

    Small deploy change

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Uploading to pypi using $PYPI_USERNAME"
 
 echo -e "[pypi]\nusername = $PYPI_USERNAME\npassword = $PYPI_PASSWORD\n" > ~/.pypirc
 
-python3 setup.py sdist bdist_wheel
+python3 setup.py build sdist bdist_wheel
 pip3 install twine
 twine upload dist/*
