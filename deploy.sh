84acd10bd38fd46ef7271d630caedefdc8c140b0
João Magalhães <joamag@gmail.com>
2020-09-16 10:08:08 +0100

    New twine installation

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ echo "Uploading to pypi using $PYPI_USERNAME"
 echo -e "[pypi]\nusername = $PYPI_USERNAME\npassword = $PYPI_PASSWORD\n" > ~/.pypirc
 
 python3 setup.py build sdist
-pip3 install twine
-twine upload dist/*
+pip3 install --upgrade twine
+python3 -m twine upload dist/*
