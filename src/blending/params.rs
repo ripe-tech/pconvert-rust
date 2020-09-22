4e985f7886565c412f128279d56e86eb8f5df19c
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-22 17:09:57 +0100

    Conditional compile enum variants

--- src/blending/params.rs
+++ src/blending/params.rs
@@ -5,9 +5,13 @@ pub type BlendAlgorithmParams = HashMap<String, ParamValue>;
 #[derive(Clone, Debug)]
 pub enum ParamValue {
     Bool(bool),
-    Int(i32),
     Long(i64),
     Float(f64),
     Str(String),
+
+    #[cfg(not(target_arch = "wasm32"))]
+    Int(i32),
+    
+    #[cfg(target_arch = "wasm32")]
     Invalid,
 }
