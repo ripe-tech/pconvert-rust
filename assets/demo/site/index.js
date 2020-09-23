7757e451eec8d7a7afa2872f0333154c07d990cc
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-23 16:53:20 +0100

    Improve demo visuals and UI to fully test WASM API

--- assets/demo/site/index.js
+++ assets/demo/site/index.js
@@ -1,13 +1,14 @@
 const js = import("./node_modules/pconvert_rust/pconvert_rust.js");
 
 const canvas = document.querySelector("canvas#composition");
-const input = document.querySelector("input#files");
-const button = document.querySelector("button#metadata");
-const textarea = document.querySelector("textarea#metadata");
+const inputFiles = document.querySelector("input#files");
+const inputAlgorithm = document.querySelector("input#algorithm");
+const textareaAlgorithms = document.querySelector("textarea#algorithms");
 const apiFunctionSelect = document.querySelector("div#api select");
+const metadata = document.querySelector("pre#metadata");
+const blendButton = document.querySelector("button#blend");
 
-input.addEventListener("change", () => execute());
-button.addEventListener("click", () => printPConvertMetadata());
+blendButton.addEventListener("click", () => blend());
 
 const API_FUNCTIONS = {
   blend_images_data: "blend_images_data",
@@ -16,50 +17,59 @@ const API_FUNCTIONS = {
   blend_multiple: "blend_multiple"
 };
 
-async function execute() {
+async function blend() {
   const pconvert = await js.then(js => js);
-  const apiFunction = apiFunctionSelect.options[apiFunctionSelect.selectedIndex].value;  
-  switch(apiFunction) {
+  const apiFunction = apiFunctionSelect.options[apiFunctionSelect.selectedIndex].value;
+  switch (apiFunction) {
     case API_FUNCTIONS.blend_images_data: {
-      const top = getImageData(await loadImage(input.files[0]));
-      const bot = getImageData(await loadImage(input.files[1]));
-      const composition = pconvert.blend_images_data(top, bot)
+      const top = getImageData(await loadImage(inputFiles.files[0]));
+      const bot = getImageData(await loadImage(inputFiles.files[1]));
+      const algorithm = inputAlgorithm.value;
+      const composition = pconvert.blend_images_data(top, bot, algorithm);
       drawComposition(composition);
       break;
     }
 
     case API_FUNCTIONS.blend_images: {
-      const top = input.files[0];
-      const bot = input.files[1];
-      const composition = await pconvert.blend_images(top, bot);
+      const top = inputFiles.files[0];
+      const bot = inputFiles.files[1];
+      const algorithm = inputAlgorithm.value;
+      const composition = await pconvert.blend_images(top, bot, algorithm);
       drawComposition(composition);
       break;
     }
 
     case API_FUNCTIONS.blend_multiple_data: {
       const data = [];
-      for(file of input.files){
+      for (file of inputFiles.files) {
         const imageData = getImageData(await loadImage(file));
         data.push(imageData);
       }
-      // Example of a possible 'algorithms' and 'params' list
-      // let algorithms = ["alpha", "multiplicative", {
-      //   algorithm: "mask_top",
-      //   params: {
-      //     factor: 0.8,
-      //     tobias: true,
-      //     alberto: "caeiro",
-      //     integer: 3,
-      //   }
-      // }];
-      const composition = pconvert.blend_multiple_data(data);
-      drawComposition(composition);
+
+      const algorithm = inputAlgorithm.value;
+      if (algorithm == "") {
+        const algorithms = JSON.parse(textareaAlgorithms.value)["algorithms"];
+        const composition = pconvert.blend_multiple_data(data, null, algorithms);
+        drawComposition(composition);
+      }
+      else {
+        const composition = pconvert.blend_multiple_data(data, algorithm);
+        drawComposition(composition);
+      }
       break;
     }
 
     case API_FUNCTIONS.blend_multiple: {
-      const composition = await pconvert.blend_multiple(input.files);
-      drawComposition(composition);
+      const algorithm = inputAlgorithm.value;
+      if (algorithm == "") {
+        const algorithms = JSON.parse(textareaAlgorithms.value)["algorithms"];
+        const composition = await pconvert.blend_multiple(inputFiles.files, null, algorithms);
+        drawComposition(composition);
+      }
+      else {
+        const composition = await pconvert.blend_multiple(inputFiles.files, algorithm);
+        drawComposition(composition);
+      }
       break;
     }
 
@@ -68,10 +78,10 @@ async function execute() {
   }
 }
 
-async function printPConvertMetadata() {
+async function getPConvertMetadata() {
   const pconvert = await js.then(js => js);
   const constants = pconvert.get_module_constants();
-  textarea.value = JSON.stringify(constants, undefined, 4);
+  metadata.innerHTML = JSON.stringify(constants, undefined, 2);
 }
 
 function loadImage(url) {
@@ -100,3 +110,5 @@ function drawComposition(composition) {
   const context = canvas.getContext("2d");
   context.putImageData(composition, 0, 0);
 }
+
+getPConvertMetadata()
