84c886d4a9614f6faabd28eda3ceffa5b0d65493
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-23 11:20:44 +0100

    Add mtpng crate and write_png_parallel

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,7 @@ use super::errors::PConvertError;
 use image::io::Reader;
 use image::png::{CompressionType, FilterType, PngEncoder};
 use image::{ColorType, DynamicImage, ImageBuffer, ImageFormat, Rgba};
+use mtpng;
 use std::fs::File;
 use std::io::BufWriter;
 
@@ -35,6 +36,31 @@ pub fn write_png(
     let buff = BufWriter::new(file);
     let encoder = PngEncoder::new_with_quality(buff, compression, filter);
     Ok(encoder.encode(&png, png.width(), png.height(), ColorType::Rgba8)?)
+}
+
+pub fn write_png_parallel(
+    file_out: String,
+    png: &ImageBuffer<Rgba<u8>, Vec<u8>>,
+    _compression: CompressionType,
+    _filter: FilterType,
+) -> Result<(), PConvertError> {
+    let writer = File::create(file_out)?;
+
+    let mut header = mtpng::Header::new();
+    header.set_size(png.width(), png.height())?;
+    header.set_color(mtpng::ColorType::TruecolorAlpha, 8)?;
+
+    let options = mtpng::encoder::Options::new();
+    // options.set_compression_level(level: CompressionLevel)
+    // options.set_filter_mode(filter_mode: Mode<Filter>)
+
+    let mut encoder = mtpng::encoder::Encoder::new(writer, &options);
+
+    encoder.write_header(&header)?;
+    encoder.write_image_rows(&png)?;
+    encoder.finish()?;
+
+    Ok(())
 }
 
 pub fn max<T: PartialOrd>(x: T, y: T) -> T {
