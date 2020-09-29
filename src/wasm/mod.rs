bdb6ded01a64c991d4d783759b1531607f167558
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-09-29 16:36:04 +0100

    refactor blend_images and blend_images_data

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -12,7 +12,7 @@ use crate::blending::{
 use crate::constants;
 use crate::errors::PConvertError;
 use crate::utils::encode_png;
-use image::{ImageBuffer, RgbaImage};
+use image::{ImageBuffer, Rgba, RgbaImage};
 use js_sys::{try_iter, Array, Uint8Array};
 use serde_json::json;
 use utils::{
@@ -40,26 +40,21 @@ pub async fn blend_images(
     algorithm: Option<String>,
     is_inline: Option<bool>,
 ) -> Result<File, JsValue> {
-    let algorithm = algorithm.unwrap_or(String::from("multiplicative"));
-    let algorithm = build_algorithm(&algorithm)?;
-    let algorithm_fn = get_blending_algorithm(&algorithm);
-    let demultiply = is_algorithm_multiplied(&algorithm);
+    let mut top = load_png(top, false).await?;
+    let mut bot = load_png(bot, false).await?;
 
-    let top = load_png(top, demultiply).await?;
-    let mut bot = load_png(bot, demultiply).await?;
+    blend_image_buffers(&mut top, &mut bot, algorithm, is_inline)?;
 
-    blending::blend_images(&top, &mut bot, &algorithm_fn, &None);
-
-    let mut data = bot.to_vec();
+    let mut encoded_data = Vec::<u8>::with_capacity(bot.to_vec().capacity());
     encode_png(
-        &mut data[..],
+        &mut encoded_data,
         &bot,
         image::png::CompressionType::Default,
         image::png::FilterType::NoFilter,
     )?;
 
     unsafe {
-        let array_buffer = Uint8Array::view(&data);
+        let array_buffer = Uint8Array::view(&encoded_data);
         File::new_with_u8_array_sequence(&Array::of1(&array_buffer), "result.png")
     }
 }
@@ -71,14 +66,6 @@ pub fn blend_images_data(
     algorithm: Option<String>,
     is_inline: Option<bool>,
 ) -> Result<ImageData, JsValue> {
-    let algorithm = algorithm.unwrap_or(String::from("multiplicative"));
-    let algorithm = build_algorithm(&algorithm)?;
-
-    let _is_inline = is_inline.unwrap_or(false);
-
-    let algorithm_fn = get_blending_algorithm(&algorithm);
-    let demultiply = is_algorithm_multiplied(&algorithm);
-
     let (width, height) = (top.width(), top.height());
     let mut top: RgbaImage = ImageBuffer::from_vec(width, height, top.data().to_vec())
         .ok_or_else(|| PConvertError::ArgumentError("Could not parse \"top\"".to_string()))?;
@@ -86,17 +73,34 @@ pub fn blend_images_data(
     let mut bot: RgbaImage = ImageBuffer::from_vec(width, height, bot.data().to_vec())
         .ok_or_else(|| PConvertError::ArgumentError("Could not parse \"bot\"".to_string()))?;
 
-    if demultiply {
-        demultiply_image(&mut top);
-        demultiply_image(&mut bot);
-    }
 
-    blending::blend_images(&top, &mut bot, &algorithm_fn, &None);
+    blend_image_buffers(&mut top, &mut bot, algorithm, is_inline)?;
 
     let bot_bytes = &mut bot.to_vec();
     let clamped_bot_bytes: Clamped<&mut [u8]> = Clamped(bot_bytes);
     let result = ImageData::new_with_u8_clamped_array_and_sh(clamped_bot_bytes, width, height)?;
     Ok(result)
+}
+
+fn blend_image_buffers(
+    top: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
+    bot: &mut ImageBuffer<Rgba<u8>, Vec<u8>>,
+    algorithm: Option<String>,
+    is_inline: Option<bool>,
+) -> Result<(), PConvertError> {
+    let algorithm = algorithm.unwrap_or(String::from("multiplicative"));
+    let algorithm = build_algorithm(&algorithm)?;
+    let algorithm_fn = get_blending_algorithm(&algorithm);
+    let demultiply = is_algorithm_multiplied(&algorithm);
+    let _is_inline = is_inline.unwrap_or(false);
+
+    if demultiply {
+        demultiply_image(top);
+        demultiply_image(bot);
+    }
+
+    blending::blend_images(&top, bot, &algorithm_fn, &None);
+    Ok(())
 }
 
 #[wasm_bindgen]
