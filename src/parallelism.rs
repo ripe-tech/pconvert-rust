8f3a7992397c335c8f46d05be8ab360d7ecd8602
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-10-01 11:03:30 +0100

    Add ThreadPoolStatus struct

--- src/parallelism.rs
+++ src/parallelism.rs
@@ -1,5 +1,6 @@
 use crate::errors::PConvertError;
 use image::{ImageBuffer, Rgba};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread::{spawn, JoinHandle};
 
@@ -7,6 +8,7 @@ pub struct ThreadPool {
     workers: Vec<Worker>,
     work_channel_sender: mpsc::Sender<WorkMessage>,
     work_channel_receiver_mutex: Arc<Mutex<mpsc::Receiver<WorkMessage>>>,
+    status: Arc<ThreadPoolStatus>,
 }
 
 impl ThreadPool {
@@ -20,17 +22,19 @@ impl ThreadPool {
         let workers = Vec::with_capacity(size);
         let work_channel_receiver_mutex = Arc::new(Mutex::new(work_channel_receiver));
 
+        let status = Arc::new(ThreadPoolStatus::new(size));
+
         Ok(ThreadPool {
             workers,
             work_channel_sender,
             work_channel_receiver_mutex,
+            status,
         })
     }
 
     pub fn start(&mut self) {
         for _ in 0..self.workers.capacity() {
-            self.workers
-                .push(Worker::new(Arc::clone(&self.work_channel_receiver_mutex)));
+            self.spawn_worker();
         }
     }
 
@@ -58,7 +62,24 @@ impl ThreadPool {
             .send(WorkMessage::NewTask(task, result_channel_sender))
             .unwrap_or_default();
 
+        self.status.inc_queued_count();
+
         result_channel_receiver
+    }
+
+    // pub fn expand_to(&mut self, num_threads: usize) {
+    //     let to_spawn = num_threads - self.status.size();
+    //     for _ in 0..to_spawn {
+    //         self.spawn_worker();
+    //         self.status.inc_size();
+    //     }
+    // }
+
+    fn spawn_worker(&mut self) {
+        self.workers.push(Worker::new(
+            self.status.clone(),
+            Arc::clone(&self.work_channel_receiver_mutex),
+        ));
     }
 }
 
@@ -73,17 +94,25 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(receiver: Arc<Mutex<mpsc::Receiver<WorkMessage>>>) -> Worker {
+    fn new(
+        thread_pool_status: Arc<ThreadPoolStatus>,
+        receiver: Arc<Mutex<mpsc::Receiver<WorkMessage>>>,
+    ) -> Worker {
         let thread = spawn(move || loop {
             let message = receiver.lock().unwrap().recv().unwrap();
             match message {
                 WorkMessage::NewTask(task, result_channel_sender) => {
+                    thread_pool_status.dec_queued_count();
+                    thread_pool_status.inc_active_count();
+
                     let result = task();
                     result_channel_sender.send(result).unwrap_or_default();
+
+                    thread_pool_status.dec_active_count();
                 }
 
                 WorkMessage::Terminate => {
-                    break;
+                    thread_pool_status.dec_size();
                 }
             }
         });
@@ -102,4 +131,56 @@ enum WorkMessage {
 
 pub enum ResultMessage {
     ImageResult(Result<ImageBuffer<Rgba<u8>, Vec<u8>>, PConvertError>),
+}
+
+struct ThreadPoolStatus {
+    size: AtomicUsize,
+    queued_count: AtomicUsize,
+    active_count: AtomicUsize,
+}
+
+impl ThreadPoolStatus {
+    pub fn new(size: usize) -> Self {
+        ThreadPoolStatus {
+            size: AtomicUsize::new(size),
+            queued_count: AtomicUsize::new(0),
+            active_count: AtomicUsize::new(0),
+        }
+    }
+
+    // pub fn size(&self) -> usize {
+    //     self.size.load(Ordering::Relaxed)
+    // }
+
+    // pub fn queued(&self) -> usize {
+    //     self.queued_count.load(Ordering::Relaxed)
+    // }
+
+    // pub fn active(&self) -> usize {
+    //     self.active_count.load(Ordering::Relaxed)
+    // }
+
+    pub fn inc_queued_count(&self) {
+        self.queued_count.fetch_add(1, Ordering::Relaxed);
+    }
+
+    pub fn dec_queued_count(&self) {
+        self.queued_count.fetch_sub(1, Ordering::Relaxed);
+    }
+
+    pub fn inc_active_count(&self) {
+        self.active_count.fetch_add(1, Ordering::Relaxed);
+    }
+
+    pub fn dec_active_count(&self) {
+        self.active_count.fetch_sub(1, Ordering::Relaxed);
+    }
+
+    // pub fn inc_size(&self) {
+    //     self.size.fetch_add(1, Ordering::Relaxed);
+    // }
+
+    pub fn dec_size(&self) {
+        self.size.fetch_sub(1, Ordering::Relaxed);
+    }
 }
