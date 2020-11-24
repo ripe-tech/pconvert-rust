9d5081a67a6ac1808bb2bfd77c94a2d8f6bb307f
João Magalhães <joamag@gmail.com>
2020-11-24 22:25:37 +0000

    Version 0.2.7

--- pconvert_rust/test/global.py
+++ pconvert_rust/test/global.py
@@ -9,4 +9,4 @@ class GlobalTest(unittest.TestCase):
 
     def test_basic(self):
         self.assertEqual(type(pconvert_rust.VERSION), str)
-        self.assertEqual(pconvert_rust.VERSION, "0.2.6")
+        self.assertEqual(pconvert_rust.VERSION, "0.2.7")
