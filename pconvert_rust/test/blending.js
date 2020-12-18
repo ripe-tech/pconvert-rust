b71b5c7981ee1d68cb9e5a5c8a2848a9e375a200
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-18 14:25:58 +0000

    Add blendMultipleFs test

--- pconvert_rust/test/blending.js
+++ pconvert_rust/test/blending.js
@@ -1,5 +1,8 @@
 const assert = require("assert");
+const path = require("path");
 const pconvert = require("pconvert-rust");
+
+const TEST_ASSETS = path.resolve("assets/test");
 
 describe("NodeJS WASM", async function() {
     it("should get module constants with specific keys", () => {
@@ -19,5 +22,19 @@ describe("NodeJS WASM", async function() {
         ];
         keys.forEach(k => assert(k in constants));
         Object.keys(constants).forEach(k => keys.includes(k));
+    });
+
+    it("should blend multiple files from local file system", () => {
+        const paths = [
+            path.resolve(`${TEST_ASSETS}/sole.png`),
+            path.resolve(`${TEST_ASSETS}/back.png`),
+            path.resolve(`${TEST_ASSETS}/front.png`),
+            path.resolve(`${TEST_ASSETS}/shoelace.png`),
+            path.resolve(`${TEST_ASSETS}/background_alpha.png`)
+        ];
+        const out = path.resolve(`${TEST_ASSETS}/result_alpha_alpha_Fast_NoFilter.png`);
+        const algorithm = "alpha";
+        const algorithms = null;
+        pconvert.blendMultipleFs(paths, out, algorithm, algorithms, true);
     });
 });
