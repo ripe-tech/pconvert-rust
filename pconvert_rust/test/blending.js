964b4bad4a779347f2de7134bc6a826daa4e1319
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-18 14:37:11 +0000

    Async tests, ensure out exists

--- pconvert_rust/test/blending.js
+++ pconvert_rust/test/blending.js
@@ -1,5 +1,6 @@
-const assert = require("assert");
+const fs = require("fs");
 const path = require("path");
+const assert = require("assert");
 const pconvert = require("pconvert-rust");
 
 const TEST_ASSETS = path.resolve("assets/test");
@@ -21,7 +22,7 @@ describe("NodeJS WASM", async function() {
             "VERSION"
         ];
         keys.forEach(k => assert(k in constants));
-        Object.keys(constants).forEach(k => keys.includes(k));
+        Object.keys(constants).forEach(k => assert(keys.includes(k)));
     });
 
     it("should blend multiple files from local file system", () => {
@@ -36,5 +37,21 @@ describe("NodeJS WASM", async function() {
         const algorithm = "alpha";
         const algorithms = null;
         pconvert.blendMultipleFs(paths, out, algorithm, algorithms, true);
+        assert(fs.existsSync(out));
+    });
+
+    it("should blend multiple files from local file system in an async fashion", async () => {
+        const paths = [
+            path.resolve(`${TEST_ASSETS}/sole.png`),
+            path.resolve(`${TEST_ASSETS}/back.png`),
+            path.resolve(`${TEST_ASSETS}/front.png`),
+            path.resolve(`${TEST_ASSETS}/shoelace.png`),
+            path.resolve(`${TEST_ASSETS}/background_alpha.png`)
+        ];
+        const out = path.resolve(`${TEST_ASSETS}/result_alpha_alpha_Fast_NoFilter.png`);
+        const algorithm = "alpha";
+        const algorithms = null;
+        await pconvert.blendMultipleFsAsync(paths, out, algorithm, algorithms, true);
+        assert(fs.existsSync(out));
     });
 });
