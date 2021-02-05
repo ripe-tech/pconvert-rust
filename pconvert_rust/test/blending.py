0336bfd5afcbc422d4b8296584c0b164a99c4fe3
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-02-05 15:20:36 +0000

    test: add more tests

--- pconvert_rust/test/blending.py
+++ pconvert_rust/test/blending.py
@@ -9,6 +9,22 @@ TEST_ASSETS = os.path.join(os.path.dirname(__file__), "../../assets/test/")
 
 class BlendingTest(unittest.TestCase):
 
+    def test_module_constants(self):
+        mandatory = [
+            "ALGORITHMS",
+            "COMPILATION_DATE",
+            "COMPILATION_TIME",
+            "COMPILER",
+            "COMPILER_VERSION",
+            "COMPRESSION_TYPES",
+            "FEATURES",
+            "FILTER_TYPES",
+            "LIBPNG_VERSION",
+            "PLATFORM_CPU_BITS",
+            "VERSION"
+        ];
+        for key in mandatory: self.assertTrue(hasattr(pconvert_rust, key))
+
     def test_blend_images(self):
         pconvert_rust.blend_images(
             os.path.abspath(f"{TEST_ASSETS}sole.png"),
@@ -72,6 +88,7 @@ class BlendingTest(unittest.TestCase):
         )
 
     def test_blend_multiple(self):
+        # default algorithms
         pconvert_rust.blend_multiple(
             (
                 os.path.abspath(f"{TEST_ASSETS}sole.png"),
@@ -83,7 +100,7 @@ class BlendingTest(unittest.TestCase):
             os.path.abspath(f"{TEST_ASSETS}result_alpha_alpha_Fast_NoFilter.png")
         )
 
-    def test_blend_multiple_multithread(self):
+        # explicit algorithms
         pconvert_rust.blend_multiple(
             (
                 os.path.abspath(f"{TEST_ASSETS}sole.png"),
@@ -93,6 +110,38 @@ class BlendingTest(unittest.TestCase):
                 os.path.abspath(f"{TEST_ASSETS}background_alpha.png")
             ),
             os.path.abspath(f"{TEST_ASSETS}result_alpha_alpha_Fast_NoFilter.png"),
+            algorithm = "alpha",
+            algorithms = ["alpha", "multiplicative", "destination_over", "source_over"]
+        )
+
+    def test_blend_multiple_multithread(self):
+        # default algorithms
+        pconvert_rust.blend_multiple(
+            (
+                os.path.abspath(f"{TEST_ASSETS}sole.png"),
+                os.path.abspath(f"{TEST_ASSETS}back.png"),
+                os.path.abspath(f"{TEST_ASSETS}front.png"),
+                os.path.abspath(f"{TEST_ASSETS}shoelace.png"),
+                os.path.abspath(f"{TEST_ASSETS}background_alpha.png")
+            ),
+            os.path.abspath(f"{TEST_ASSETS}result_alpha_alpha_Fast_NoFilter.png"),
+            options = {
+                "num_threads": 5
+            }
+        )
+
+        # explicit algorithms
+        pconvert_rust.blend_multiple(
+            (
+                os.path.abspath(f"{TEST_ASSETS}sole.png"),
+                os.path.abspath(f"{TEST_ASSETS}back.png"),
+                os.path.abspath(f"{TEST_ASSETS}front.png"),
+                os.path.abspath(f"{TEST_ASSETS}shoelace.png"),
+                os.path.abspath(f"{TEST_ASSETS}background_alpha.png")
+            ),
+            os.path.abspath(f"{TEST_ASSETS}result_alpha_alpha_Fast_NoFilter.png"),
+            algorithm = "alpha",
+            algorithms = ["alpha", "multiplicative", "destination_over", "source_over"],
             options = {
                 "num_threads": 5
             }
