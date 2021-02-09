3cebb845838e2e57d61cd7c75c9bf03d81e06bfe
João Magalhães <joamag@gmail.com>
2021-02-09 14:31:51 +0000

    version: 0.4.10

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
 
 setuptools.setup(
     name = "pconvert-rust",
-    version = "0.4.9",
+    version = "0.4.10",
     author = "Platforme International",
     author_email = "development@platforme.com",
     description = "PNG Convert Rust",
