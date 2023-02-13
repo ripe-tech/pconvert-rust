aff4f83c90ef5a3ebe27a2204f639ed2babd6312
João Conde <joaodiasconde@gmail.com>
2023-02-13 15:58:33 +0000

    style: black

--- pconvert_rust/test/global.py
+++ pconvert_rust/test/global.py
@@ -5,8 +5,8 @@ import unittest
 
 import pconvert_rust
 
-class GlobalTest(unittest.TestCase):
 
+class GlobalTest(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(type(pconvert_rust.VERSION), str)
         self.assertEqual(pconvert_rust.VERSION, "0.4.11")
